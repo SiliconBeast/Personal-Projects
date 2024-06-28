307d3819533a8a6e48ec82c4329fe8bcf462184a
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-06-27 21:27:33 -0600

    Add files via upload
    New Python Project

---
+++ Love Calculator Program.py
@@ -0,0 +1,24 @@
+print("The Love Calculator is calculating your score...")
+name1 = "S"#"What is your name? "
+name2 = "A"#What is their name? "
+# Write your code below this line 👇
+added_name = name1 + name2
+lower_name = added_name.lower()
+t = lower_name.count("t")
+r = lower_name.count("r")
+u = lower_name.count("u")
+e = lower_name.count("e")
+l = lower_name.count("l")
+o = lower_name.count("o")
+v = lower_name.count("v")
+e = lower_name.count("e")
+first_digit = t + r + u + e
+second_digit = l + o + v + e
+num_score = int(str(first_digit) + str(second_digit))
+
+if num_score < 10 or num_score > 90:
+    print(f"Your score is {num_score}, you go together like coke and mentos.")
+elif 40 <= num_score <= 50:
+    print(f"Your score is {num_score}, you are alright together.")
+else:
+    print(f"Your score is {num_score}.")
