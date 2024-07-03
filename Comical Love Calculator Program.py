6513c9505ca8f0f876a9b536c8067a117d369686
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-02 19:29:44 -0600

    Update Comical Love Calculator Program.py

--- Comical Love Calculator Program.py
+++ Comical Love Calculator Program.py
@@ -1,3 +1,6 @@
+#lol this is just a fun program I learnt in a Python course based on some internet article.
+#not to be taken seriously in any means :P
+
 print("The Love Calculator is calculating your score...")
 name1 = "S"#"What is your name? "
 name2 = "A"#What is their name? "
@@ -16,6 +19,7 @@ first_digit = t + r + u + e
 second_digit = l + o + v + e
 num_score = int(str(first_digit) + str(second_digit))
 
+# now for calculation to be specified in the filters
 if num_score < 10 or num_score > 90:
     print(f"Your score is {num_score}, you go together like coke and mentos.")
 elif 40 <= num_score <= 50:
