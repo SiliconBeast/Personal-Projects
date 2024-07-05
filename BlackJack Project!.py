b444819eaf294aef4ba77a54ec2d62d99dbf122b
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-04 19:50:17 -0600

    Add files via upload

---
+++ BlackJack Project!.py
@@ -0,0 +1,26 @@
+#Let's make a BlackJack Project!
+user_cards = []
+computer_cards = []
+def deal_card():
+    cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+    return cards
+
+def calculate_score(cards):
+    sum the inputs
+    return score
+    if deal_cards = ace +10 then
+    return 0 #represents blackjack
+    if score is over 21, replace 11 with 1
+calculate_score()
+
+while calculate_score = True:
+    Run 
+    else
+    ask for another card
+    use deal_card to add another card 
+    
+
+
+
+
+    
