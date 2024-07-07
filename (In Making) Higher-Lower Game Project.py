61882b6c4c3dd2ae4f9d148eeb5e9204979e09d0
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-06 19:08:22 -0600

    Update (In Making) Higher-Lower Game Project.py

--- (In Making) Higher-Lower Game Project.py
+++ (In Making) Higher-Lower Game Project.py
@@ -1,4 +1,6 @@
-#higher-lower game project in development
+### higher-lower game project in development ###
+### the game is based on the simple rule that the player needs to guess which of the given statements has more followers ###
+### if the player is correct, they gain a point else they lose immediately ###
 print('''         __      
    / / / (_)___ _/ /_  ___  _____
   / /_/ / / __ '/ __ \/ _ \/ ___/
@@ -9,3 +11,4 @@ print('''         __
  / /___/ /_/ / |/ |/ /  __/ /    
 /_____/\____/|__/|__/\___/_/
 ''')
+
