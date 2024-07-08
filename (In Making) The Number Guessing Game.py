8fd18b4dd95b34dee33da415f9677fa1e1dca3a0
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-08 00:37:29 -0600

    Update (In Making) The Number Guessing Game.py
    partial solution

--- (In Making) The Number Guessing Game.py
+++ (In Making) The Number Guessing Game.py
@@ -1,3 +1,27 @@
 # the program is a simple game which has 2 levels
 # it gives us chances to guess the number and tells whether the guess is too high or too low
 # i used the fact of using the def function and the simple if-else statements for this personal projects :)
+import random
+
+def number_guessed():
+    print("Hi There The Player!")
+    level = input("Choose a difficulty level, Select by typing either 'easy' or 'hard': ").lower()
+    
+    if level == "easy":
+        attempts = 10
+    elif level == "hard":
+        attempts = 5
+    else:
+        print("Wrong Choice Player! Choose again to continue")
+        
+    while attempts > 0 :
+        guess = int(input("Make A Guess: "))
+        
+        if guess < number_guessed:
+            print("You are going too low")
+        elif guess > number_guessed:
+            print("You are going too high")
+        elif guess == number_guessed:
+            print("You guessed the right number!!!")
+            return
+        
