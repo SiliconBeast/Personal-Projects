3e21633b3ddfba10e3cfc6f2cfd71809f8bb0367
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-07 23:49:10 -0600

    Update (In Making) The Number Guessing Game.py

--- (In Making) The Number Guessing Game.py
+++ (In Making) The Number Guessing Game.py
@@ -1,2 +1,3 @@
 # the program is a simple game which has 2 levels
 # it gives us chances to guess the number and tells whether the guess is too high or too low
+# i used the fact of using the def function and the simple if-else statements for this personal projects :)
