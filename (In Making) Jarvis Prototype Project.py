8c30690b51eb7830ffa5beb9d8c5e3f579a95919
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-10 15:41:48 -0600

    Update (In Making) Jarvis Prototype Project.py
    - progress checkpoint 1

--- (In Making) Jarvis Prototype Project.py
+++ (In Making) Jarvis Prototype Project.py
@@ -1,1 +1,29 @@
 # Jarvis in process :)
+# Jarvis in process :)
+
+import speech_recognition as sr 
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer() #helps in speech recog. functionality
+ttsx = pyttsx3.init()
+
+# takes voice and outputs as text
+def speak():
+    engine.say(text)
+    engine.runAndWait()
+    
+    
+if __name__ == "__main__":
+    speak("Initializing Jarvis...")
+    while True:
+    # listen for the wake phrase "JARVIS"
+    # get audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening!")
+        audio = r.listen(source)
+    command = r.recog
+    # recognize the speech with the help of Sphinx
+    try:
+        print("Sphinx")
