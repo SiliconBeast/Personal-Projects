ec1fc43af8065aef4e295532ebb077cd35985851
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-11 22:57:32 -0600

    Update (In Making) Coffee Machine Project.py
    - project milestone 2

--- (In Making) Coffee Machine Project.py
+++ (In Making) Coffee Machine Project.py
@@ -79,4 +79,4 @@ while machine_is_on:
         if is_resource_sufficient(drink["ingredients"]):
             payment = process_coins()
             if is_transaction_successful(payment,drink["cost"]):
-            
+                make_coffee()
