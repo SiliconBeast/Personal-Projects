8ad8b3bb7cc55fa4f5524e8797c9eb05d4a75325
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-11 22:29:16 -0600

    Update (In Making) Coffee Machine Project.py
    -progress milestone 1

--- (In Making) Coffee Machine Project.py
+++ (In Making) Coffee Machine Project.py
@@ -1,6 +1,31 @@
 # coffee machine code
 
-choice = input(print("What would you like to drink today? (espresso/latte/cappuccino):"))
+profit = 0 #initial money is zero
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+}
 
-if choice == "espresso":
-    print("Water: 100ml")
+# now we will define a function that shows there are not enough ingredients
+def is_resource_sufficient(order_ingredients):
+    is_enough = True
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print("Sorry, there is not have enough {item}.")
+            is_enough = False
+    return True
+
+
+machine_is_on = True
+while machine_is_on:
+    choice = input("What would you like to drink today? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        machine_is_on = False
+    elif choice == "report":
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Water: {resources['water']}ml")
+        print(f"Money: ${profit}")
+    else:
+
