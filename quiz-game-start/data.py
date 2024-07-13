73d51dc20d3550a85b92532e2e4de45ae265675d
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-13 01:22:10 -0600

    Update data.py

--- quiz-game-start/data.py
+++ quiz-game-start/data.py
@@ -1,4 +1,4 @@
-question_data = [
+Question_data = [
 {"text": "A slug's blood is green.", "answer": "True"},
 {"text": "The loudest animal is the African Elephant.", "answer": "False"},
 {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
