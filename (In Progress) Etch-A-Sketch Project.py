e759937656b7401575234b4093febdd6af72d3ba
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-14 14:15:31 -0600

    Update (In Progress) Etch-A-Sketch Project.py
    syntax update 1

--- (In Progress) Etch-A-Sketch Project.py
+++ (In Progress) Etch-A-Sketch Project.py
@@ -1,3 +1,36 @@
 # We are creating etch-a-sketch using the turtle in python
 # this takes in the input from the user and rotates the turtle as per the different inputs
 
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+screen.exitonclick()
+
