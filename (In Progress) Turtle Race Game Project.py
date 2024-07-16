4c8af71057195f2c6af59ab694bc134414fdb2b5
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-07-15 21:59:08 -0600

    Update (In Progress) Turtle Race Game Project.py
    - debugging complete

--- (In Progress) Turtle Race Game Project.py
+++ (In Progress) Turtle Race Game Project.py
@@ -4,8 +4,15 @@ import random
 is_race_on = False
 screen = Screen()
 screen.setup(width=600, height=500)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win")
-colors = ["red", "green", "blue", "orange", "purple", "pink", "yellow"]
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win- Red, Green, Blue, Orange, Yellow, Purple?")
+colors = ["Red", "Green", "Blue", "Orange", "Yellow"]
+
+# Create a turtle for writing the title
+title_turtle = Turtle()
+title_turtle.hideturtle()
+title_turtle.penup()
+title_turtle.goto(0, screen.window_height()//2 - 40)
+title_turtle.write("Ninja Turtle Racing Game", align="center", font=("Arial", 24, "bold"))
 
 ### y_position = [-100, -60, -20, 20, 60, 100, 140]
 ### for turtle_index in range(0, 6):
@@ -53,19 +60,19 @@ tim7.goto(x=-250, y=140)
 if user_bet:
     is_race_on = True
 
-all_turtles = [tim, tim2, tim3, tim4, tim5, tim6]
+all_turtles = [tim, tim2, tim3, tim4, tim5, tim6, tim7]
 
 while is_race_on:
     for turtle in all_turtles:
         if turtle.xcor() > 230:
             is_race_on = False
-            print(f"{turtle.pencolor()} turtle won!")
+            print(f"{turtle.pencolor()} Turtle won!")
             if turtle.pencolor() == user_bet:
                 print("You won!")
             else:
                 print("You lost!")
 
-    random_distance = random.randint(0, 10)
-    turtle.forward(random_distance)
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
