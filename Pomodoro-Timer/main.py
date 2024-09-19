baa41d1cec281cc0b96f405591cd0a9c120607d7
Suvir <103403762+SiliconBeast@users.noreply.github.com>
2024-09-18 23:06:48 -0600

    Create main.py

---
+++ Pomodoro-Timer/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
